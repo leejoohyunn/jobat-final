a8a8979b131e750f9c4a40abb1306782519ac62d
JJadeYoon <yess9904@naver.com>
2024-07-24 11:39:58 +0900

    [DOCS] 로컬 실행 가이드 추가

--- src/utils.py
+++ src/utils.py
@@ -175,35 +175,17 @@ def crawl_data(link):
             last_height = new_height
 
     ### Chrome 브라우저를 설정하고 실행합니다. ###
-    # 로컬
+
+    ########## 로컬 ##########
     # driver = webdriver.Chrome()
 
-    # ec2
-    # driver_path = '/usr/bin/chromedriver'
-    # driver = webdriver.Chrome(driver_path)
-
-    # from selenium import webdriver
-    # from selenium.webdriver.chrome.service import Service
-    # from webdriver_manager.chrome import ChromeDriverManager
-    # from selenium.webdriver.chrome.options import Options
-    # import time
-    # from selenium.webdriver.common.by import By
-    # chrome_options = Options()
-    # chrome_options.add_argument('--headless')  # 브라우저를 머리 없는 모드로 실행하려면 주석을 해제하세요.
-    #
-    # # Service 객체를 사용하여 ChromeDriverManager를 통해 경로를 설정합니다.
-    # service = Service(ChromeDriverManager().install())
-    # driver = webdriver.Chrome(service=service, options=chrome_options)
-
+    ########## ec2 ##########
     from pyvirtualdisplay import Display
     from selenium import webdriver
     from selenium.webdriver.chrome.service import Service
     from webdriver_manager.chrome import ChromeDriverManager
 
     driver_options = webdriver.ChromeOptions()
-
-    # 헤드리스 옵션 사용 여부
-    # driver_options.add_argument("headless")
 
     # 가상 웹브라우저 설정
     display = Display(visible=0, size=(1024, 768))
