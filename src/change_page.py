087f9eeb928afe29a122ee8245c990e51bb72bb9
leejoohyunn <151380306+leejoohyunn@users.noreply.github.com>
2024-07-30 15:46:58 +0900

    Update change_page.py

--- src/change_page.py
+++ src/change_page.py
@@ -7,37 +7,34 @@ from utils import (
 
 def change_page():
     # Title for the main page
-    st.title('메인 페이지')
+    #st.title('메인 메뉴 돌아가기')
     # Central buttons
-    st.write('\n')  # Add some space before buttons
-    st.write('### 선택해주세요:')  # Header for buttons section
+    #st.write('\n')  # Add some space before buttons
+    #st.write('### 선택해주세요:')  # Header for buttons section
 
-    # Define two central buttons with some spacing in between
-    col1, col2 = st.columns([1, 1])
+    # CSS to increase button size
+    st.markdown("""
+        <style>
+        .big-button {
+            font-size: 20px !important;
+            height: 3em;
+            width: 100%;
+    }
+    </style>
+    """, unsafe_allow_html=True)
+    
+    # Button for 금기사항 물어보기
+    if st.button('금기사항 물어보기', key='button1'):
+        st.write('금기사항 관련 페이지로 이동합니다.')
+        navigate_to("chat")  # 실제 네비게이션 함수 호출
 
-    with col1:
-        next_page = st.button('금기사항 물어보기')
-        if (next_page):
-            navigate_to("chat")
-        #if st.button('금기사항 물어보기'):
-        #st.write('금기사항 관련 페이지로 이동합니다.')
-        #navigate_to("chat.py")
-        # chat.py로 이동하는 코드
-        # st.experimental_rerun() 사용 시, 선택적 리다이렉션 구현 가능
+    # Button for 의약품 정보 물어보기
+    if st.button('의약품 정보 물어보기', key='button2'):
+        st.write('의약품 정보 관련 페이지로 이동합니다.')
+        navigate_to("chat")  # 실제 네비게이션 함수 호출
 
-    with col2:
-        the_next_page = st.button('의약품 정보 물어보기')
-        if (the_next_page):
-            navigate_to("chat")
-   # if st.button('의약품 정보 물어보기'):
-    #    st.write('의약품 정보 관련 페이지로 이동합니다.')
-        # chat.py로 이동하는 코드
-        # st.experimental_rerun() 사용 시, 선택적 리다이렉션 구현 가능
-
-# Sidebar with navigation button
+    # Sidebar with navigation button
     st.sidebar.title('메뉴')
     if st.sidebar.button('메인으로 돌아가기'):
-        navigate_to("main")
-        # main.py로 돌아가는 코드
-    # st.experimental_rerun() 사용 시, 선택적 리다이렉션 구현 가능
-        st.write('메인 페이지로 돌아갑니다.')
+        st.write('메인 페이지로 돌아갑니다.')g
+        # 실제 네비게이션 코드 삽입 가능
