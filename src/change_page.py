63aeefd7decc1b8721271b849a63120abb02bc3b
leejoohyunn <151380306+leejoohyunn@users.noreply.github.com>
2024-07-30 14:56:13 +0900

    Update change_page.py

--- src/change_page.py
+++ src/change_page.py
@@ -1,4 +1,3 @@
-
 import streamlit as st
 
 # Title for the main page
@@ -12,16 +11,22 @@ st.write('### 선택해주세요:')  # Header for buttons section
 col1, col2 = st.columns([1, 1])
 
 with col1:
-    if st.button('금기사항 물어보기'):
-        st.write('금기사항 관련 페이지로 이동합니다.')
+    next_page = st.button('금기사항 물어보기')
+    if (next_page):
         navigate_to("chat")
+    #if st.button('금기사항 물어보기'):
+        #st.write('금기사항 관련 페이지로 이동합니다.')
+        #navigate_to("chat.py")
         # chat.py로 이동하는 코드
         # st.experimental_rerun() 사용 시, 선택적 리다이렉션 구현 가능
 
 with col2:
-    if st.button('의약품 정보 물어보기'):
-        st.write('의약품 정보 관련 페이지로 이동합니다.')
+    the_next_page = st.button('의약품 정보 물어보기')
+    if (the_next_page):
         navigate_to("chat")
+   # if st.button('의약품 정보 물어보기'):
+    #    st.write('의약품 정보 관련 페이지로 이동합니다.')
+        # chat.py로 이동하는 코드
         # st.experimental_rerun() 사용 시, 선택적 리다이렉션 구현 가능
 
 # Sidebar with navigation button
