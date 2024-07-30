0f89cfb957f825aa304548c4f419b9bd1ea738b3
leejoohyunn <151380306+leejoohyunn@users.noreply.github.com>
2024-07-30 14:49:11 +0900

    Update change_page.py

--- src/change_page.py
+++ src/change_page.py
@@ -14,13 +14,14 @@ col1, col2 = st.columns([1, 1])
 with col1:
     if st.button('금기사항 물어보기'):
         st.write('금기사항 관련 페이지로 이동합니다.')
+        navigate_to("chat")
         # chat.py로 이동하는 코드
         # st.experimental_rerun() 사용 시, 선택적 리다이렉션 구현 가능
 
 with col2:
     if st.button('의약품 정보 물어보기'):
         st.write('의약품 정보 관련 페이지로 이동합니다.')
-        # chat.py로 이동하는 코드
+        navigate_to("chat")
         # st.experimental_rerun() 사용 시, 선택적 리다이렉션 구현 가능
 
 # Sidebar with navigation button
